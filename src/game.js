0b5f4fe6812f782a183f84c9b74c1889cd574483
paxriel <70996104+paxriel@users.noreply.github.com>
2020-09-09 12:54:33 +0800

    Add files via upload

---
+++ src/game.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const gameSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    isCurrent: {
+        type: Boolean,
+        default: true
+    }
+})
+
+const Game = mongoose.model('Game', gameSchema)
+
+module.exports = Game
