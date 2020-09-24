9dc011e76691a1ac673cc2f3d244189eb412cb99
paxriel <70996104+paxriel@users.noreply.github.com>
2020-09-24 19:21:29 +0800

    Update section.js

--- src/router/section.js
+++ src/router/section.js
@@ -1,91 +1,112 @@
-// Author: Paxriel (https://twitch.tv/paxriel)
 const express = require('express')
-const fs = require('fs')
-var localeObject = checkLocale()
+const router = new express.Router()
 
-const Game = require('./db/game')
+const publicKey = process.env.PUBLIC_KEY
+const privateKey = process.env.PRIVATE_KEY
 
-// Checks whether the locale specified exists
-function checkLocale() {
-    const localeSpecified = process.env.LOCALE || 'en-gb'
-    try {
-        const currentObject = fs.readFileSync(`./locales/${localeSpecified}.json`)
-        return JSON.parse(currentObject)
-    } catch (e) {
-        // No translation is available for this line as well, this error would imply that the locales file could not be loaded
-        console.log(`Locale ${localeSpecified} missing or could not be opened, stack trace below`)
-        console.log(e.stack)
-        process.exit(20)
+const Section = require('../db/section')
+
+module.exports = function (localeObject, subValues, defaultError, getCurrentGame) {
+// Gets the total amount of sections in the game (Without deaths)
+/* Query parameters:
+   public_key: The public key of the API
+   game: The game for the total amount of sections (Optional, defaults to the current game)
+*/
+router.get('/getsection', async (req, res) => {
+    if (!req.query.public_key || req.query.public_key !== publicKey) {
+        return defaultError(res)
     }
-}
-
-// Check whether all the specified keys exists
-function checkKeys() {
-    if (!process.env.PRIVATE_KEY) {
-        console.log(localeObject.privateKeyMissing)
-        process.exit(21)
-    } else if (!process.env.PUBLIC_KEY) {
-        console.log(localeObject.publicKeyMissing)
-        process.exit(21)
-    } else if (!process.env.METRIC_KEY) {
-        console.log(localeObject.metricKeyMissing)
-        process.exit(21)
+    var game = req.query.game || await getCurrentGame()
+    if (!game) {
+        return res.send(localeObject.noGameSpecified)
     }
-}
+    var responseString = subValues(localeObject.sectionListPre, { game })
+    Section.find({ parent: game }, null, { sort: { created_at: 'asc' } }, (err, sectionList) => {
+        if (err) {
+            console.log(subValues(localeObject.errorGettingSectionList, { game }))
+            console.log(err.stack)
+            return defaultError(res)
+        }
 
-// Substitute a specific string with the given arguments into a different language
-function subValues(original, valuesObject={}) {
-    if (!original) {
-        return ""
-    }
-    return original.replace(/\${game}/g, valuesObject.game).replace(/\${section}/g, valuesObject.section).replace(/\${deaths}/g, valuesObject.deaths)
-}
-
-// Default error message
-function defaultError(res) {
-    return res.send(localeObject.unexpectedError)
-}
-
-// Get the current game
-async function getCurrentGame() {
-    var promise = new Promise((resolve, reject) => {
-        Game.findOne({ isCurrent: true }, (err, game) => {
-            if (err) {
-                console.log(localeObject.errorFindingGame)
-                console.log(err.stack)
-                resolve(null)
-            } else if (!game) {
-                resolve(null)
-            } else {
-                resolve(game.name)
-            }
+        sectionList.forEach((section) => {
+            responseString += (section.name + ', ')
         })
+        if (sectionList.length !== 0 ) {
+            // Remove the last ', ' at the end
+            responseString = responseString.slice(0, -2)
+        }
+        responseString += subValues(localeObject.sectionListPost, { game })
+        return res.send(responseString)
     })
-    return promise
+})
+
+// Adds a section to the game
+/* Query parameters:
+   private_key: The private key of the API
+   game: The game for the total amount of sections (Optional, defaults to the current game)
+   name: The name of the section that would be added
+*/
+router.get('/addsection', async (req, res) => {
+    if (!req.query.private_key || req.query.private_key !== privateKey) {
+        return defaultError(res)
+    } else if (!req.query.name) {
+        return res.send(localeObject.noNameSpecified)
+    }
+
+    var game = req.query.game || await getCurrentGame()
+    if (!game) {
+        return res.send(localeObject.noGameSpecified)
+    }
+    Section.findOne({ name: req.query.name, parent: game }, async (err, section) => {
+        if (err) {
+            console.log(subValues(localeObject.errorCheckingDuplicateSections, { game, section: req.query.name }))
+            console.log(err.stack)
+            return defaultError(res)
+        } else if (section) {
+            return res.send(subValues(localeObject.sectionAlreadyExists, { game, section: req.query.name }))
+        }
+
+        var newSection = new Section({
+            name: req.query.name,
+            parent: game
+        })
+        try {
+            await newSection.save()
+            return res.send(subValues(localeObject.sectionAdded, { game, section: req.query.name }))
+        } catch (e) {
+            console.log(subValues(localeObject.errorAddingSection, { game, section: req.query.name }))
+            console.log(e.stack)
+            return defaultError(res)
+        }
+    })
+})
+
+// Removes a section from the game
+/* Query parameters:
+   private_key: The private key of the API
+   game: The game for the total amount of sections (Optional, defaults to the current game)
+   name: The name of the section that would be added
+*/
+router.get('/removesection', async (req, res) => {
+    if (!req.query.private_key || req.query.private_key !== privateKey) {
+        return defaultError(res)
+    } else if (!req.query.name) {
+        return res.send(localeObject.noNameSpecified)
+    }
+    var game = req.query.game || await getCurrentGame()
+    if (!game) {
+        return res.send(localeObject.noGameSpecified)
+    }
+    Section.deleteOne({ name: req.query.name, parent: game }, (err) => {
+        if (err) {
+            console.log(subValues(localeObject.errorDeletingSection, { game, section: req.query.name }))
+            console.log(err.stack)
+            return defaultError(res)
+        } else {
+            return res.send(subValues(localeObject.sectionDeleted, { game, section: req.query.name }))
+        }
+    })
+})
+
+return router
 }
-
-checkKeys()
-require('./db/mongoose')(localeObject)
-const gameRouter = require('./router/game')(localeObject, subValues, defaultError, getCurrentGame)
-const sectionRouter = require('./router/section')(localeObject, subValues, defaultError, getCurrentGame)
-const deathsRouter = require('./router/deaths')(localeObject, subValues, defaultError, getCurrentGame)
-
-const port = process.env.PORT || 4001
-const app = express()
-
-// No robots
-app.get('/robots.txt', async (req, res) => {
-    return res.send('User-agent: \*\nDisallow: /')
-})
-
-app.use(gameRouter)
-app.use(sectionRouter)
-app.use(deathsRouter)
-
-app.get('*', (req, res) => {
-    return res.send('Server is running')
-})
-
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
